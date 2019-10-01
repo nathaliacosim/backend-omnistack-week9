cd16bdfc5daf49903e8f813e083eec8369414537
nathaliacosim <nathalia.cosim@novaandradina.org>
2019-10-01 16:20:28 -0400

    create backend

---
+++ README.md
@@ -0,0 +1,16 @@
+## Backend - OmniStack Week 9
+
+Foi usado:
+- React;
+- Insomnia;
+- Mongo Atlas;
+
+Download das dependencias:
+```javascript
+>> npm install
+```
+
+Run App:
+```javascript
+>> yarn dev
+```
---
+++ src/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+const app = express();
+mongoose.connect('mongodb+srv://<user>:<password>@omnistack-wz73p.mongodb.net/nome_banco?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// req.query = Acessar query params (filtros)
+// req.params = Acessar route params (edição, exclusão)
+// req.body = Acessar corpo da requisição (criação, edição)
+
+app.use(express.json());
+app.use(routes);
+app.listen(3030);
